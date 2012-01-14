c2749fe8e38a070f6120ac020b8bd88736c07e5f
Fabian Vogler <fabian@equivalence.ch>
2012-01-15 00:28:58 +0100

    Use category model

--- category.php
+++ category.php
@@ -2,12 +2,12 @@
 
 require_once 'includes/session.php';
 require_once 'includes/database.php';
+require_once 'includes/webshop.php';
 
 $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
-$statement = $database->prepare('SELECT * FROM `categories` WHERE `id` = ?');
-$statement->execute(array($id));
-$category = $statement->fetch();
+$categories = new Categories($database);
+$category = $categories->get($id);
 
 if (!$category) {    
     header('Location: ./'); // redirect to home if category is not found
