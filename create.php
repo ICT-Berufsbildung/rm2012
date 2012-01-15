0b4aec9708f890c933617b216675bdf9d972d69e
Fabian Vogler <fabian@equivalence.ch>
2012-01-15 01:28:09 +0100

    Added TODO

---
+++ create.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'includes/session.php';
+require_once 'includes/database.php';
+require_once 'includes/webshop.php';
+
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
+
+// TODO implement save order
