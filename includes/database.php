bb8667227fbb3e80d581fe992671a68467cf3649
Fabian Vogler <fabian@equivalence.ch>
2012-01-16 21:56:55 +0100

    Improved instructions

--- includes/database.php
+++ includes/database.php
@@ -9,5 +9,7 @@ try {
     $database = new PDO('mysql:dbname=' . $dbname . ';host=' . $host, $user, $password);
     $database->exec("SET NAMES 'utf8'");
 } catch (PDOException $e) {
-    echo 'Please check file includes/database.php, because database connection failed: ' . $e->getMessage();
+    echo 'Create a database "webshop", import the file webshop.sql and check the file includes/database.php, database connection failed: ' . $e->getMessage();
+    echo '';
+    exit;
 }
