95fd62d8924ca54fe7b323266d660bbd7b5ed62f
Fabian Vogler <fabian@equivalence.ch>
2012-01-16 01:53:53 +0100

    Added hint about debugging code

--- create.php
+++ create.php
@@ -4,6 +4,7 @@ require_once 'includes/session.php';
 require_once 'includes/database.php';
 require_once 'includes/webshop.php';
 
+// Debugging code, remove at will
 echo '<pre>';
 var_dump($_POST);
 echo '</pre>';
