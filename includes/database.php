2b9656fcb47c4e4c1c9f81fda253fa65e5bee468
Fabian Vogler <fabian@equivalence.ch>
2012-01-16 21:51:52 +0100

    Easier database configuration

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,12 @@
 <?php
 
-$dsn = 'mysql:dbname=webshop;host=localhost';
+$host = 'localhost';
+$dbname = 'webshop';
 $user = 'root';
 $password = '';
 
 try {
-    $database = new PDO($dsn, $user, $password);
+    $database = new PDO('mysql:dbname=' . $dbname . ';host=' . $host, $user, $password);
     $database->exec("SET NAMES 'utf8'");
 } catch (PDOException $e) {
     echo 'Please check file includes/database.php, because database connection failed: ' . $e->getMessage();
