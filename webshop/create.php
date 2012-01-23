c1500af44536e697ce5aa84be809317dcc6f833e
Fabian Vogler <fabian@equivalence.ch>
2012-01-24 00:53:04 +0100

    Link back home

--- webshop/create.php
+++ webshop/create.php
@@ -9,6 +9,8 @@ require_once 'includes/database.php';
 require_once 'includes/webshop.php';
 
 // Debugging code, remove at will
+echo '<h2>Not implemented yet.</h2>';
+echo '<p><a href="./">Back to Home page.</a></p>';
 echo '<pre>';
 var_dump($_POST);
 echo '</pre>';
