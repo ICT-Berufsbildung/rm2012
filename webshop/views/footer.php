8f8f694784ab045bbe609e3b5acd8c340261ba9a
Fabian Vogler <fabian@equivalence.ch>
2012-01-28 14:05:34 +0100

    Added version number

--- webshop/views/footer.php
+++ webshop/views/footer.php
@@ -1,7 +1,7 @@
             <footer class="footer">
                 <p>
                     Competitor: <?php display($competitor); ?><br/>
-                    &copy; <?php echo date('Y'); ?> Jungfraubahnen Management AG
+                    &copy; <?php echo date('Y'); ?> Jungfraubahnen Management AG, Version 1.2
                 </p>
             </footer>
         </div>
