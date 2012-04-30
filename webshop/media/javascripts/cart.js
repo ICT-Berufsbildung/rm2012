00cb0cefcdd555bf84286abd382c42f89d3d6bad
Fabian Vogler <fabian@equivalence.ch>
2012-05-01 01:17:42 +0200

    Task 4

--- webshop/media/javascripts/cart.js
+++ webshop/media/javascripts/cart.js
@@ -1,12 +1,12 @@
 $(function () {
 
-    $('.form-cart .remove').click(function () {
+    $('.form-cart .remove').live('click', function () {
 
         // get url from link
         var url = $(this).attr('href');
 
         // remove product and reload cart view
-        $('.form-cart').load(url + ' .form-cart');
+        $('.container').load(url + ' .container > *');
 
         // prevent default
         return false;
